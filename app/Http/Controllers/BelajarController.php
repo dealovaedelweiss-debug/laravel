413336c406c24fc0c3683b5a007994f08e20b92e
dealovaedelweiss-debug <ppkdjwebdealovaedelweiss@gmail.com>
2026-08-10 14:56:57 +0700

    10 agustus senin

--- app/Http/Controllers/BelajarController.php
+++ app/Http/Controllers/BelajarController.php
@@ -2,11 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class BelajarController extends Controller
 {
+    public function index()
+    {
+        return view('counting');
+    }
+
+    public function indexKurang()
+    {
+        // $nilai1 = 10;
+        // $nilai2 = 6;
+        // $hasil = $nilai1 - $nilai2;
+
+        return view('kurang');
+    }
+
+    public function indexkali()
+    {
+        // $nilai1 = 10;
+        // $nilai2 = 6;
+        // $hasil = $nilai1 - $nilai2;
+
+        return view('kali');
+    }
+
     // extends adalah turunan
     public function greeting()
     {
         return 'Selamat datang di kelas laravel';
+    }
+
+    public function tambah()
+    {
+        $nilai1 = 5;
+        $nilai2 = 7;
+        $hasil = $nilai1 + $nilai2;
+
+        return "$nilai1 + $nilai2 = $hasil";
+    }
+
+    public function kurang(Request $request)
+    {
+        $angka1 = $request->angka1;
+        $angka2 = $request->angka2;
+        $hasil = $angka1 - $angka2;
+
+        return view('kurang', compact('hasil'));
+        // compact melempar hasil ke view kurangh
+    }
+
+    public function kali(Request $request)
+    {
+        // $nilai1 = 90;
+        // $nilai2 = 4;
+        // $hasil = $nilai1 * $nilai2;
+        $angka1 = $request->angka1;
+        $angka2 = $request->angka2;
+        $hasil = $angka1 * $angka2;
+
+        return view('kali', compact('hasil'));
+    }
+
+    public function bagi()
+    {
+        $nilai1 = 16;
+        $nilai2 = 2;
+        $hasil = $nilai1 / $nilai2;
+
+        return "$nilai1 / $nilai2 = $hasil";
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@ Route::get('/', function () {
 // POST : mengirim data dari form, dan aksinya adalah insert
 // PUT : mengirim data dari form yang aksinya untuk update
 // DELETE : untuk menghapus data yang ada
-// PATCH : sama seperti PUT namun hanya bisa 1 data kalo PUT bisa untuk semua data
+// PATCH : sama seperti PUT namun hanya bisa 1 data kalo PUT bisa untuk semua data aksi nya update
+Route::get('counting', [BelajarController::class, 'index']);
 
 Route::get('salam', [BelajarController::class, 'greeting']);
+
+Route::get('hitung-tambah', [BelajarController::class, 'tambah'])->name('hitung-tambah');
+// kalau menggunakan route di views harus menggunakan name seperti diatas harus sama dengan yang di route
+Route::get('hitung-kurang', [BelajarController::class, 'indexKurang']);
+Route::post('action-kurang', [BelajarController::class, 'kurang'])->name('action-kurang');
+// get untuk memunculkan hasil
+Route::get('hitung-kali', [BelajarController::class, 'indexkali']);
+Route::post('action-kali', [BelajarController::class, 'kali'])->name('action-kali
+');
+Route::get('hitung-bagi', [BelajarController::class, 'bagi']);
